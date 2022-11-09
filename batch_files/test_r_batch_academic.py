d5c89590400310ee1346774dbf840c5aab4ad8d3
SEEWON <andyaaa78@gmail.com>
2022-11-09 16:01:54 +0900

    [Feat] Apply Improved algorithm draft: Handle notices cutting in the middle/Multiple notice added simultaneously

---
+++ batch_files/test_r_batch_academic.py
@@ -0,0 +1,16 @@
+import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+import revise_selenium_crawl
+
+#def exec_batch():
+refer_file = 'webcat/top_notices/test_notices.txt'
+slack_channel = 'dev-feature'
+detecting_website = 'http://www.sogang.ac.kr/front/boardlist.do?bbsConfigFK=2'
+notice_board_html_tree = 'div.grid.bbs-list.tr-hover'
+each_notice_html_tree = 'div.subject > a'
+domain='https://www.sogang.ac.kr'
+
+revise_selenium_crawl.crawl_all_notices_firstpage(refer_file, slack_channel, detecting_website, notice_board_html_tree, each_notice_html_tree, domain)
+
+#revise_selenium_crawl.read_file(refer_file)
